270d0761f0a3fb85bb9267d8d32cac2cc2b986ee
miladiashe <dkdud0122@naver.com>
2021-02-02 21:29:52 +0900

    첫 번째 실습. 비쥬얼 스튜디오 환경에서는 파일 이름을 바꾸고 시작 파일로 지정하면 꼬이는건가? 잘 모르겠으니 일단 파일 이름은 안 건드리고 진행한다.

--- Doit_Flask_study/Doit_Flask_study/__init__.py
+++ Doit_Flask_study/Doit_Flask_study/__init__.py
@@ -5,4 +5,8 @@ The flask application package.
 from flask import Flask
 app = Flask(__name__)
 
-import Doit_Flask_study.views
+@app.route('/')
+def hello_pybo():
+    return 'Hello, Pybo!'
+
+# import Doit_Flask_study.views
