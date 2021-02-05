25d78fec0689d4117a4b415697b5025a18bf25b7
miladiashe <dkdud0122@naver.com>
2021-02-06 01:10:16 +0900

    살려주세ㅕ요

--- Doit_Flask_study/Doit_Flask_study/__init__.py
+++ Doit_Flask_study/Doit_Flask_study/__init__.py
@@ -5,8 +5,4 @@ The flask application package.
 from flask import Flask
 app = Flask(__name__)
 
-@app.route('/')
-def hello_pybo():
-    return 'Hello, Pybo!'
-
-# import Doit_Flask_study.views
+import Doit_Flask_study.views
